04d189b2222cddd9a3e262dd576839b2b313a3d8
David C. Bradley <david@dbradley771.com>
2020-04-11 17:51:24 -0500

    Update docs script to use correct branch

--- scripts/initDocsFolder.sh
+++ scripts/initDocsFolder.sh
@@ -2,7 +2,7 @@
 # Initialize Docs Folder.
 set -v
 REPO="https://github.com/UNO-CSCI4970-SP20-BOB/TiMES-UI.git"
-mkdir ../docs
-cd ../docs
-git clone --single-branch --branch docs $REPO .
+mkdir ./docs
+cd ./docs
+git clone --single-branch --branch gh-pages $REPO .
 exit 0
