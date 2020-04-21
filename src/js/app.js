3dbb97a38a0e964f2471041f1bee67af0872a189
David C. Bradley <david@dbradley771.com>
2020-04-21 03:21:20 -0500

    Add event card design and update events list

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,32 @@
+import { MDCList } from '@material/list';
+import { MDCRipple } from '@material/ripple';
+import { MDCTopAppBar } from '@material/top-app-bar';
 
 /**
  * Initializes page.
  */
 function init() {
     // Page is ready to run JavaScript
+    console.debug('init()');
+
+    const appBarSelector = document.querySelector('.mdc-top-app-bar');
+    if (appBarSelector) {
+        new MDCTopAppBar(appBarSelector);
+    }
+
+    [].map.call(document.querySelectorAll('.mdc-button, .mdc-card__primary-action'), function (element) {
+        return new MDCRipple(element);
+    });
+
+    [].map.call(document.querySelectorAll('.mdc-icon-button'), function (element) {
+        const iconButtonRipple = new MDCRipple(element);
+        iconButtonRipple.unbounded = true;
+        return iconButtonRipple;
+    });
+
+    [].map.call(document.querySelectorAll('.mdc-list'), function (element) {
+        return new MDCList(element);
+    });
 }
 
 
@@ -12,9 +35,9 @@ function init() {
  */
 function pageReady() {
     console.debug('pageReady()');
-    document.removeEventListener("DOMContentLoaded", pageReady);
-    window.removeEventListener("load", pageReady);
+    document.removeEventListener('DOMContentLoaded', pageReady);
+    window.removeEventListener('load', pageReady);
     init();
 }
-document.addEventListener("DOMContentLoaded", pageReady);
-window.addEventListener("load", pageReady);
+document.addEventListener('DOMContentLoaded', pageReady);
+window.addEventListener('load', pageReady);
